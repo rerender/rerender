f9cb7c16f0eb3e8ee05af3638c840ef72d03094f
Vasiliy Lector <vasiliy.lector@gmail.com>
2018-03-03 19:29:41 +0300

    renderDOM

--- src/renderDOM.ts
+++ src/renderDOM.ts
@@ -8,65 +8,151 @@ import { TemplateFragment } from './TemplateFragment';
 
 type DOMNode = HTMLElement | Document;
 
-type Patch = {
-    type: 'create' | 'move' | 'update' | 'remove',
+type MovableNode = {
+    domNode: HTMLElement,
+    template: Template<string>
+};
+
+type ComponentNode = {
+    component: Component<any>,
+    parentComponent?: Component<any>,
+    template?: Template, // only for moveable
+    props: any,
+    state?: any,
+    tree: Renderable
+};
+
+type MapNodes = Map<DOMNode | MovableNode | ComponentNode>;
+
+type Patch = PatchCreate | PatchMove | PatchRemove | PatchUpdate;
+
+type PatchCreate = {
+    type: 'create',
     parentDomNode: DOMNode,
-    id: string,
+    tryReuseDOM?: boolean,
     domIndex: number,
-    replace?: boolean,
-    domNode?: HTMLElement | DocumentFragment
+    domNode: HTMLElement | DocumentFragment
+    changedNodes: MapNodes
+};
+
+type PatchMove = {
+    type: 'move',
+    parentDomNode: DOMNode,
+    domIndex: number,
+    domNodes: [HTMLElement],
+    changedNodes: MapNodes
+};
+
+type PatchRemove = {
+    type: 'remove',
+    domNodes: [HTMLElement]
+    removedNodes: Map<boolean>,
+    templateForRemove: Renderable
+};
+
+type PatchUpdate = {
+    type: 'update',
+    domNode: HTMLElement,
+    setProps: Map<any>,
+    removeProps: Map<true>,
+    changedNodes: MapNodes
 };
 
 type Context = {
     id: string,
     parentDomNode: DOMNode,
-    templateIndex: number,
-    nextDomIndex: number
+    parentComponent?: Component<any>,
+    nextDomIndex: number,
+    insidePatchType?: 'create' | 'move' | 'remove',
+    insidePatchWithReplace?: boolean
 };
 
-type Instance = {
-    component?: Component<any>,
-    state?: any,
-    render: Renderable
-};
+type Next = (patch: Patch) => any;
 
-type RenderDOMOptions = {
-    templatesById: Map<Renderable>,
-    instancesById: Map<Instance>,
-    nextTemplatesById: Map<Renderable>,
-    nextInstancesById: Map<Instance>
-};
-
-function patching(
-    template: Renderable,
-    prevTemplateByPosition: Renderable,
+function render(
+    nextTemplate: Renderable,
+    prevTemplate: Renderable,
     context: Context,
-    options: RenderDOMOptions
+    nodesById: MapNodes,
+    next: Next
 ) {
-    const prevTemplateById = options.templatesById[context.id];
-    if (template) {
-        if (!prevTemplateById) {
-            if (!prevTemplateByPosition) {
-                // create
-            } else {
-                // create and replace
-            }
-        } else {
-            if (!prevTemplateByPosition) {
+    // if (nextTemplate != null) {
+        // if (prevTemplate == null) {
+            // if (isMovable(nextTemplate) && isSameType(template, prevTemplateById)) {
                 // move
-            } else if (prevTemplateById !== prevTemplateByPosition) {
-                // move and replace
-            }
-        }
-        if (template instanceof Template) {
+            // } else {
+                // create
+            // }
+        // } else {
+            // if (!isSameType(template, prevTemplate)) {
+                // if (isMovable(nextTemplate) && isSameType(template, prevTemplateById)) {
+                    // move and replace
+                // } else {
+                    // replace
+                // }
+            // }
+        // }
+        // patch deeper here
+    // } else {
+        // if (prevTemplate != null) {
+            // remove prev and meta
+        // }
+    // }
 
+    if (typeof nextTemplate === 'object') {
+        if (nextTemplate instanceof Template) {
+            if (typeof nextTemplate.componentType === 'string') {
+                // renderElement(nextTemplate, prevTemplate, context, nodesById);
+            } else if (nextTemplate.componentType.prototype instanceof Component) {
+                // renderComponent(nextTemplate, prevTemplate, context, nodesById);
+            } else if ((nextTemplate.componentType as StatelessComponent<any>).$uberComponent) {
+                // renderUber(nextTemplate, prevTemplate, context, nodesById);
+            } else {
+                // renderStateless(nextTemplate, prevTemplate, context, nodesById);
+            }
+        } else if (nextTemplate instanceof TemplateFragment) {
+            if (Array.isArray(nextTemplate.children)) {
+                // renderArray(nextTemplate, prevTemplate, context, nodesById);
+            } else {
+                // render(nextTemplate, prevTemplate, context, nodesById);
+            }
+        } else if (Array.isArray(nextTemplate)) {
+            // renderArray(nextTemplate, prevTemplate, context, nodesById);
+        } else if (nextTemplate === null) {
+            renderNothing(prevTemplate, context, nodesById, next);
+        } else {
+            throw new Error(
+                'Objects are not valid as Rerender child ' +
+                `(found: object ${JSON.stringify(nextTemplate)}). ` +
+                'If you meant to render a collection of children, use an array instead.'
+            );
         }
+    } else if (typeof nextTemplate === 'string') {
+        // renderString(nextTemplate, prevTemplate, context, nodesById);
+    } else if (typeof nextTemplate === 'number') {
+        // renderNumber(nextTemplate, prevTemplate, context, nodesById);
     } else {
-        if (prevTemplateById) {
-            // destroy id
-        }
-        if (prevTemplateByPosition) {
-            // remove another instance or moved
-        }
+        renderNothing(prevTemplate, context, nodesById, next);
     }
- }
+}
+
+function renderNothing(
+    prevTemplate: Renderable,
+    context: Context,
+    nodesById: MapNodes,
+    next: Next
+) {
+    if (prevTemplate != null && typeof prevTemplate !== 'boolean') {
+        // remove prev component or nodes
+    }
+}
+
+const enabledKeysTypes: Map<boolean> = {
+    string: true,
+    number: true
+};
+
+function isMovable(template: Renderable) {
+    return template instanceof Template && template.props != null &&
+        (enabledKeysTypes[typeof template.props.uniqid] || enabledKeysTypes[typeof template.props.uniqid]);
+}
